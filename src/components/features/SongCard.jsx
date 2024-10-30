9acdb6336d795c87c8fcbbe1545febfa7c3699cc
Pranay Vishwakarma <149870314+Pranay0083@users.noreply.github.com>
2024-10-31 02:08:08 +0530

    style updates 3 (onHover buttons)

--- src/components/features/SongCard.jsx
+++ src/components/features/SongCard.jsx
@@ -1,44 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { FaPlus, FaCheck, FaEllipsisV, FaPlay } from "react-icons/fa";
 
 const SongCard = ({ song, onPlay, isLiked }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div
-      className='flex-shrink-0 cursor-pointer transition-transform mr-4 group'
+      className="flex-shrink-0 cursor-pointer transition-transform mr-4 group"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <div className='w-[175px]'>
-        <div className='relative flex'>
+      <div className="w-[175px]">
+        <div className="relative flex">
           <img
             src={song.thumbnail}
             alt={`${song.title} thumbnail`}
-            className='h-[175px] w-[175px] rounded-md object-cover mb-2 group-hover:brightness-50 transition-all duration-300'
+            className="h-[175px] w-[175px] rounded-md object-cover mb-2 group-hover:brightness-50 transition-all duration-300"
           />
           <div
-            className={`absolute right-12 top-1/2 -translate-y-1/2 bg-black/20 p-2 rounded-full
-              transition-all duration-300 backdrop-blur-md h-20 w-20
-              ${isHovered ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-4'}`}
+            className={`absolute right-0 top-0 p-2 flex justify-between items-center rounded-md
+              transition-all duration-300 h-[175px] w-[175px] bg-black bg-opacity-50
+              ${isHovered ? "opacity-100" : "opacity-0"}`}
           >
-            {isLiked ? (
-              <i className="fa-solid fa-check text-green-500 absolute top-8"></i>
-            ) : (
-              <i className="fa-solid fa-plus text-gray-500 absolute top-8"></i>
-            )}
-            <i className="fa-solid fa-play w-6 h-6 text-white bg-transparent absolute right-5 top-8 cursor-pointer" onClick={onPlay}></i>
-            <i className="fa-solid fa-ellipsis-vertical text-gray-500 absolute top-8 right-2"></i>
+            <div className="flex flex-row justify-center items-center w-full h-full space-x-5">
+              {isLiked ? (
+                <FaCheck className="text-green-500 w-4 h-4 cursor-pointer" />
+              ) : (
+                <FaPlus className="text-white w-4 h-4 cursor-pointer" />
+              )}
+              <div className=" bg-black/20 rounded-full transition-all  backdrop-blur-md w-16 h-16 flex justify-center items-center" >
+                <FaPlay className="w-6 h-6 text-white cursor-pointer" />
+              </div>
+              <FaEllipsisV className="w-4 h-4 text-white cursor-pointer" />
+            </div>
           </div>
         </div>
         <div className="px-1">
-          <h2 className='text-white font-medium text-sm mb-1 truncate'>
+          <h2 className="text-white font-medium text-sm mb-1 truncate">
             {song.title}
           </h2>
-          <div className='text-gray-500 text-xs'>
+          <div className="text-gray-500 text-xs">
             {song.artist.map((artist, index) => (
               <span key={artist._id}>
                 {artist.name}
-                {index < song.artist.length - 1 ? ', ' : ''}
+                {index < song.artist.length - 1 ? ", " : ""}
               </span>
             ))}
           </div>
